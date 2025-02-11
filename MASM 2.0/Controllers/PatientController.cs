7faaabf65841f8d3a0b137dd11857736d0372dda
Erwin WebriQ <info@webriq.com>
2025-02-12 02:00:39 +0800

    Controller and View for Patient

---
+++ MASM 2.0/Controllers/PatientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MASM_2._0.Controllers
+{
+	public class PatientController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
