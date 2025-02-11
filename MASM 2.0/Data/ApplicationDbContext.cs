1fd80fb666182fdc7a4804df9b1d4751194cebf4
Erwin WebriQ <info@webriq.com>
2025-02-12 01:52:09 +0800

    init connectiopn

---
+++ MASM 2.0/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MASM_2._0.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		 public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+		}
+	}
+}
---
+++ MASM 2.0/Models/Patient.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MASM_2._0.Models
+{
+	public class Patient
+	{
+		public int Id { get; set; }
+
+		[Required]
+		public string FirstName { get; set; }
+
+		[Required]
+		public string LastName { get; set; }
+
+		[Required]
+		public string Address { get; set; }
+
+		[Required]
+		public DateTime BirthDate { get; set; }
+
+		[Required]
+		public string Sex { get; set; }
+
+		[Required]
+		public string MobileNumber { get; set; }
+
+		[Required]
+		public string Email { get; set; }
+
+		[Required]
+		public string Password { get; set; }
+
+		[Required]
+		public bool EmailVerified { get; set; }
+
+		[Required]
+		public string Status { get; set; }
+
+		[Required]
+		public string CivilStatus { get; set; }
+
+		[Required]
+		public string BloodType { get; set; }
+
+		[Required]
+		public string EmergencyContact { get; set; }
+
+		[Required]
+		public string EmergencyContactNumber { get; set; }
+	}
+}
